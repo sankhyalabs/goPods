71846786716254c41064ccc60e1fbe7d411d5c7a
Highlander Dantas <haylander60@gmail.com>
2019-05-20 17:07:07 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strconv"
+	"time"
+
+	"golang.org/x/crypto/ssh"
+)
+
+func main() {
+	nodes := getNodes()
+	signer := getChavePPK()
+	config := configureSSHClient(signer)
+
+	for _, node := range nodes {
+
+		session := getSession(node, config)
+
+		for number := 0; number < getAmountContainers(session); number++ {
+			startContainers(session, 5)
+		}
+	}
+
+}
+
+/*
+	SSH access settings
+*/
+func configureSSHClient(signer ssh.Signer) *ssh.ClientConfig {
+	return &ssh.ClientConfig{
+		User: "rancher",
+		Auth: []ssh.AuthMethod{
+			ssh.PublicKeys(signer),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+}
+
+/*
+	Get the ssh key and set up a signer for connection
+*/
+func getChavePPK() ssh.Signer {
+	pk, _ := ioutil.ReadFile("../goPods/.ssh/ssh-testes.pem")
+	signer, err := ssh.ParsePrivateKey(pk)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return signer
+}
+
+/*
+   Returns all nodes that will start the containers
+*/
+func getNodes() []string {
+	return []string{"54.91.241.253:22"}
+}
+
+/*
+	Starts containers in status stopped, based on quantity spent on subscription @{quantity}
+*/
+func startContainers(session *ssh.Session, quantidade int) {
+	defer session.Close()
+
+	var b bytes.Buffer
+	session.Stdout = &b
+	command := fmt.Sprintf("docker start $(docker ps -a --last %d --quiet --filter \"status=exited\")", quantidade)
+
+	if err := session.Run(command); err != nil {
+		log.Println("Erro:", err.Error())
+	}
+
+	fmt.Println("Iniciando", quantidade, "containers")
+	fmt.Println(b.String())
+	time.Sleep(15 * time.Minute)
+
+	fmt.Println("Esperando 15 minutos para iniciar mais ", quantidade, "containers")
+}
+
+/*
+	Realiza a conexão ssh com o node especificado na assintura do metodo
+*/
+func getSession(node string, config *ssh.ClientConfig) *ssh.Session {
+	client, err := ssh.Dial("tcp", node, config)
+
+	if err != nil {
+		panic("Failed to dial: " + err.Error())
+	}
+
+	session, err := client.NewSession()
+
+	if err != nil {
+		panic("Failed to create session: " + err.Error())
+	}
+
+	return session
+}
+
+/*
+	Checks the number of containers in the node
+*/
+func getAmountContainers(session *ssh.Session) int {
+	var b bytes.Buffer
+	session.Stdout = &b
+
+	if err := session.Run("docker ps -a | wc -l"); err != nil {
+		log.Println("Erro:", err.Error())
+	}
+
+	fmt.Println(b.String())
+	amount, _ := strconv.ParseInt(b.String(), 10, 64)
+
+	return int(amount)
+}
