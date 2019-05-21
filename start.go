a422c3cbea788294cf6ed60af94e8ce758911305
Highlander Dantas <haylander60@gmail.com>
2019-05-21 15:26:13 -0300

    save.

--- start.go
+++ start.go
@@ -3,27 +3,36 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"goPods/utils"
+	"io/ioutil"
 	"log"
 	"strconv"
+	"strings"
 	"time"
 
+	"github.com/aws/aws-lambda-go/lambda"
 	"golang.org/x/crypto/ssh"
 )
 
 func main() {
+	lambda.Start(initiating)
+}
+
+/*
+	Starting Application
+*/
+func initiating() {
 	fmt.Println("Starting Application ======= goPods ")
 
-	nodes := utils.GetNodes()
-	signer := utils.GetPPKKey()
-	config := utils.ConfigureSSHClient(signer)
+	nodes := getNodes()
+	signer := getPPKKey()
+	config := configureSSHClient(signer)
 
 	for _, node := range nodes {
 
-		session := utils.GetSession(node, config)
+		amount := getAmountContainers(getSession(node, config))
 
-		for number := 0; number < getAmountContainers(session); number++ {
-			startContainers(session, 5)
+		for number := 0; number < amount; number++ {
+			startContainers(getSession(node, config), 5)
 		}
 	}
 
@@ -43,17 +52,20 @@ func startContainers(session *ssh.Session, amount int) {
 		log.Println("Erro:", err.Error())
 	}
 
-	fmt.Println("Initiating", amount, "containers")
+	fmt.Println("\n#############################")
+	fmt.Println("Initiating", amount, "containers\n")
 	fmt.Println(b.String())
+	fmt.Println("Waiting 15 minutes to start", amount, "more containers")
 	time.Sleep(15 * time.Minute)
 
-	fmt.Println("Waiting 15 minutes to start", amount, "more containers")
 }
 
 /*
 	Checks the number of containers in the node
 */
 func getAmountContainers(session *ssh.Session) int {
+	defer session.Close()
+
 	var b bytes.Buffer
 	session.Stdout = &b
 
@@ -61,8 +73,62 @@ func getAmountContainers(session *ssh.Session) int {
 		log.Println("Erro:", err.Error())
 	}
 
-	fmt.Println(b.String())
-	amount, _ := strconv.ParseInt(b.String(), 10, 64)
-
+	number := strings.Replace(b.String(), "\n", " ", 2)
+	amount, _ := strconv.ParseInt(strings.TrimSpace(number), 10, 64)
 	return int(amount)
+}
+
+/*
+	SSH access settings
+*/
+func configureSSHClient(signer ssh.Signer) *ssh.ClientConfig {
+	return &ssh.ClientConfig{
+		User: "rancher",
+		Auth: []ssh.AuthMethod{
+			ssh.PublicKeys(signer),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+}
+
+/*
+	Get the ssh key and set up a signer for connection
+*/
+func getPPKKey() ssh.Signer {
+	pk, _ := ioutil.ReadFile("ssh-testes.pem")
+
+	signer, err := ssh.ParsePrivateKey(pk)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return signer
+}
+
+/*
+	Returns all nodes that will start the containers
+*/
+func getNodes() []string {
+	return []string{"34.204.90.20:22"}
+}
+
+/*
+	Realiza a conexão ssh com o node especificado na assintura do metodo
+*/
+func getSession(node string, config *ssh.ClientConfig) *ssh.Session {
+
+	client, err := ssh.Dial("tcp", node, config)
+
+	if err != nil {
+		panic("Failed to dial: " + err.Error())
+	}
+
+	session, err := client.NewSession()
+
+	if err != nil {
+		panic("Failed to create session: " + err.Error())
+	}
+
+	return session
 }
